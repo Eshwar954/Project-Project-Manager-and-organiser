9c4207210ec72fa9093a6a981b7bce58d0f77ecc
Eshwar954 <eshwarsaikandimalla@gmail.com>
2026-04-15 13:02:29 +0530

    cors base fix

--- smartboard/src/main/java/com/smartboard/smartboard/config/SecurityConfig.java
+++ smartboard/src/main/java/com/smartboard/smartboard/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173","https://*.onrender.com"));
+        cfg.setAllowedOrigins(List.of("http://localhost:5173","https://project-project-manager-and-organiser.onrender.com"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(false);
